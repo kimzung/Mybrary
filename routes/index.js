e48b4021ed2bad88aada63b9cd4e5752fb6efede
kimzung <zungptk@epu.edu.vn>
2021-07-29 13:55:03 +0700

    xong buoc 1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const Book = require('../models/book')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     //res.send('Hello world! This is my first web')
-    res.render('index')
+    let books 
+    try {
+        books = await Book.find().sort({createAt: 'desc'}).limit(10).exec()
+    } catch {
+        books = []
+    }
+    res.render('index', {books: books})
 })
 
-module.exports = router
+module.exports = router 
